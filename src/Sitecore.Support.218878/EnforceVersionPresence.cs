b5cfddadd9c234b434b79c4a4d04d159234d8aa5
Ivan Sheyenko <ivs@sitecore.net>
2018-04-26 14:26:28 -0400

    patch created

---
+++ src/Sitecore.Support.218878/EnforceVersionPresence.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Data.Items;
+using Sitecore.Globalization;
+
+namespace Sitecore.Support.Pipelines.ItemProvider.GetItem
+{
+  public class EnforceVersionPresence: Sitecore.Pipelines.ItemProvider.GetItem.EnforceVersionPresence
+  {
+    protected override bool IsItemEnforceVersionPresenceEnabled(Item item)
+    {
+      if (item != null&&item.Language == Language.Invariant)
+      {
+        return false;
+      }
+
+      return base.IsItemEnforceVersionPresenceEnabled(item);
+    }
+  }
+}
---
+++ src/Sitecore.Support.218878/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.218878")]
+[assembly: AssemblyProduct("Sitecore.Support.218878")]
+[assembly: ComVisible(false)]
